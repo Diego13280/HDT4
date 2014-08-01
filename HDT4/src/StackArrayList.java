d7f04db859fa35a769d2275c1793dc230ed7046c
Daniel <happy@Danny-2.local>
2014-08-01 07:39:56 -0600

    a
    s

--- HDT4/src/StackArrayList.java
+++ HDT4/src/StackArrayList.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author happy
+ * Diego Juarez 13
+ * Daniel Mejia 13271
  */
 public class StackArrayList {
     
--- HDT4/src/principal.java
+++ HDT4/src/principal.java
@@ -1,9 +1,6 @@
-
 /**
- *
  * Diego Juarez 13
  * Daniel Mejia 13271
- * 
  */
 public class principal {
     public static void main(String[] args) {
