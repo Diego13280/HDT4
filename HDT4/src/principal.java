022b610ac3b0cf1ef589781bacefa5fda8b8e3b2
Daniel <happy@Danny-2.local>
2014-08-01 07:26:18 -0600

    ver
    a

--- HDT4/src/principal.java
+++ HDT4/src/principal.java
@@ -1,19 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Administrador
+ * Diego Juarez 13
+ * Daniel Mejia 13271
+ * 
  */
 public class principal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
         
     }
     
