dff59f22c50e28a66169689841fd517ad69e238b
Diego13280 <jua13280@uvg.edu.gt>
2014-08-01 08:04:56 -0600

    fsafasdf

--- HDT4/src/Simple.java
+++ HDT4/src/Simple.java
@@ -1,14 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
  *
  * @author Diego Juarez     13280
  * @author Daniel Mejia     13271
  */
 public class Simple {
+    
     
 }
